37c0542025595996a56fdcc129280cc4bbdc6a7e
Benno Steinkamp <benno.buster@gmail.com>
2021-02-04 09:57:54 +0100

    class diagram added

--- project/src/de/tempmodulename/client/Akkuvita.java
+++ project/src/de/tempmodulename/client/Akkuvita.java
@@ -24,6 +24,7 @@ public class Akkuvita implements EntryPoint {
         tabPanel.setWidth("100%");
 
         addFilteredSearchPanel(tabPanel);
+        addFilteredSearchPanel(tabPanel);
 
         tabPanel.selectTab(0);
 
